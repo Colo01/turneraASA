1a4f5efe5818cd5a2551b96e519037d85194c3af
Colo01 <cvlytoxd@gmail.com>
2024-12-03 20:35:08 -0300

    botones

--- client/src/views/Welcome.jsx
+++ client/src/views/Welcome.jsx
@@ -1,47 +1,25 @@
-// client/src/views/Welcome.jsx
-
 import React from "react";
 import { Link } from "react-router-dom";
 import CustomNavbar from "../commons/CustomNavbar";
-import parseJwt from "../hooks/parseJwt";
 import style from "../styles/Welcome.module.css";
 
 const Welcome = () => {
-  
-
-      const token = JSON.parse(localStorage.getItem("user")).data.token;
-      const payload = parseJwt(token)
-      //console.log(payload)
-      if(!payload.admin && !payload.operator)
-      return (
+  return (
     <>
       <CustomNavbar />
       <div className={style.mainContainer}>
-        <div className={style.logoContainer}>
-          <img
-            className={style.largeLogo}
-            src={require("../images/1.png")}
-            alt="miTurno"
-          />
-          <img
-            className={style.smallLogo}
-            src={require("../images/2.png")}
-            alt="miTurno"
-          />
-        </div>
-          <div className={style.contentContainer}>
-              <div>
-                  <Link to="/calendar" style={{textDecoration:"none"}}>
-                      <h3>Sacar un turno</h3>
-                  </Link>
-              </div>
-              <div >
-                  <Link to="/myappointments" style={{textDecoration:"none"}}>
-                      <h3>Ver mis turnos</h3>
-                  </Link>
-              </div>
-              
+        <div className={style.contentContainer}>
+          <div>
+            <Link to="/calendar" className={style.button}>
+              Sacar un turno
+            </Link>
           </div>
+          <div>
+            <Link to="/myappointments" className={style.button}>
+              Ver mis turnos
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
